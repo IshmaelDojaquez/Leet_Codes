386f2f4b7d2132c7854761dab8ac02f07491948f
Ishmael Dojaquez <77464884+IshmaelDojaquez@users.noreply.github.com>
2021-02-25 12:40:22 -0500

    Update 9_Program.cs

--- 9_Program.cs
+++ 9_Program.cs
@@ -2,9 +2,13 @@ public class Solution
 {
     public bool IsPalindrome(int x) 
     {
+        //int to string
         string origin = x.ToString();
+        //string to char []
         char [] reverse = origin.ToCharArray();
+        //reverse of char []
         Array.Reverse(reverse);
+        //new string from reverse of char []
         string palindrone = new string(reverse);
 
         if (palindrone == origin)
