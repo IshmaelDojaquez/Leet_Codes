1d1f12fb14a113a82fdcc80a97b779317c156e29
Ishmael Dojaquez <77464884+IshmaelDojaquez@users.noreply.github.com>
2021-02-25 12:37:39 -0500

    Update 1688_Program.cs

--- 1688_Program.cs
+++ 1688_Program.cs
@@ -2,19 +2,22 @@
 {
     public int NumberOfMatches(int n)
     {
+        //Create counter
         int totalMatches = 0;
 
         while (n > 1)
         {
+            //If matches is an even number
             if (n % 2 == 0)
             {
                 n = n / 2;
                 totalMatches += n;
             }
+            //If matches is an odd number
             else
             {
-                n = (n - 1) / 2 + 1;
-                totalMatches += (n - 1);
+                n = (n - 1) / 2 + 1;  // Removes player who automatically moves forward and then replaces them inside tournament
+                totalMatches += (n - 1); // -1 because there was one player who automatically moved forward
             }
         }
         return totalMatches;
